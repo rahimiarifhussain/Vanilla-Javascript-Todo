0704087a51e3574bae3a1833bc952e2989751cdb
= <arifhussain.rahimi84@gmail.com>
2021-01-19 08:48:59 +0430

    add app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// Selector
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// fuction
+function addTodo(event) {
+//  console.log("hello");  /// hello
+// Prevent form submitting
+event.preventDefault();
+//Todo div
+const todoDiv = document.createElement('div');
+todoDiv.classList.add('todo');
+// Create Li
+const newTodo = document.createElement('li');
+newTodo.innerText = todoInput.value;
+newTodo.classList.add('todo-item');
+todoDiv.appendChild(newTodo);
+// CEHCK MARK Button
+const compoletedButton = document.createElement('button');
+compoletedButton.innerHTML = "<i class='fas fa-check'></i>";
+compoletedButton.classList.add('complete-btn');
+todoDiv.appendChild(compoletedButton);
+// CEHCK trash Button
+const TrashButton = document.createElement('button');
+TrashButton.innerHTML = "<i class='fas fa-trash'></i>";
+TrashButton.classList.add('trash-btn');
+todoDiv.appendChild(TrashButton);
+// APEND to LIST
+todoList.appendChild(todoDiv);
+// clear todo input value
+todoInput.value="";
+}
